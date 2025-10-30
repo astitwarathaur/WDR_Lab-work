a49784e55bb5e19645086343d8b990d88695024e
Astitwa Rathaur <astitwarathaur639@gmail.com>
2025-10-30 12:44:11 +0530

    pass data from parent to child

---
+++ LAB-30OCT/App.jsx
@@ -0,0 +1,12 @@
+import Faculty from "./Faculty.jsx";
+
+function App() {
+
+  return (
+    <div>
+      <Faculty facultyName="Ramesh" subject="Javaa" experience="13 years" />
+    </div>
+  )
+}
+
+export default App
